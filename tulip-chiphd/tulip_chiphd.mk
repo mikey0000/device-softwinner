8d567bb5a35a06c85c4e4c22836d049e2543a8f9
Kamil Trzcinski <ayufan@ayufan.eu>
2016-08-25 02:16:51 +0200

    Android TV with HDMI-CEC driver

--- tulip-chiphd/tulip_chiphd.mk
+++ tulip-chiphd/tulip_chiphd.mk
@@ -1,12 +1,19 @@
-$(call inherit-product, device/softwinner/tulip-common/tulip_64_bit.mk)
-$(call inherit-product, build/target/product/full_base.mk)
+$(call inherit-product, device/softwinner/tulip-common/tulip_32_bit.mk)
+$(call inherit-product, device/google/atv/products/atv_generic.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/board/generic/device.mk)
 $(call inherit-product, device/softwinner/tulip-common/tulip-common.mk)
 $(call inherit-product-if-exists, device/softwinner/tulip-chiphd/modules/modules.mk)
 
 DEVICE_PACKAGE_OVERLAYS := device/softwinner/tulip-chiphd/overlay \
                            $(DEVICE_PACKAGE_OVERLAYS)
 
-PRODUCT_PACKAGES += Launcher3
+$(call inherit-product, vendor/google/atv/atv-vendor.mk)
+
+PRODUCT_PACKAGES += \
+    Launcher3 \
+    AtvRemoteService \
+    LeanbackLauncher \
+    TV
 
 PRODUCT_PACKAGES += \
     Update \
@@ -14,6 +21,9 @@ PRODUCT_PACKAGES += \
     ESFileExplorer \
     VideoPlayer \
     Bluetooth
+
+PRODUCT_PACKAGES += \
+    hdmi_cec.tulip
 
 PRODUCT_COPY_FILES += \
     device/softwinner/tulip-chiphd/kernel:kernel \
@@ -35,6 +45,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.hdmi.cec.xml:system/etc/permissions/android.hardware.hdmi.cec.xml \
+    frameworks/native/data/etc/android.software.live_tv.xml:system/etc/permissions/android.software.live_tv.xml
 
 PRODUCT_COPY_FILES += \
     device/softwinner/tulip-chiphd/configs/camera.cfg:system/etc/camera.cfg \
@@ -73,8 +87,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.zygote.disable_gl_preload=false
 
+# xhdpi, while we are hardcoding the 1080 resolution.
+# when we start doing 720 as well, will need to stop hardcoding this.
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=160 \
+    ro.sf.lcd_density=320 \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.spk_dul.used=false \
@@ -98,10 +114,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
         ro.softmouse.midbtn.code=-1 \
         ro.softmouse.rightbtn.code=-1
 
-PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hdmi.device_type=4
 
-PRODUCT_AAPT_CONFIG := mdpi large xlarge
-PRODUCT_AAPT_PREF_CONFIG := mdpi
+PRODUCT_AAPT_CONFIG := normal large xlarge hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.app_widgets.xml:system/etc/permissions/android.software.app_widgets.xml
 
 $(call inherit-product-if-exists, vendor/google/products/gms_5.1r4_db.mk)
 
