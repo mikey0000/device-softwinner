d6f74611037107e6c03bfed3ce27d5d88aaf7216
Kamil Trzcinski <ayufan@ayufan.eu>
2016-09-04 23:39:38 +0200

    Optimise power

--- tulip-chiphd/power/power_tulip.c
+++ tulip-chiphd/power/power_tulip.c
@@ -55,7 +55,7 @@
 
 /*  value define */
 #define ROOMAGE_PERF       "816000 4 0 0 1152000 4 0 0 0"
-#define ROOMAGE_NORMAL     "0 2 0 0 1152000 4 0 0 0"
+#define ROOMAGE_NORMAL     "0 4 0 0 1152000 4 0 0 0"
 #define ROOMAGE_VIDEO      "0 4 0 0 1152000 4 0 0 0"
 #define ROOMAGE_LOWPOWER   "0 1 0 0 1152000 4 0 0 0"
 
