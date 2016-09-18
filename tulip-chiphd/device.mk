0309bd8eca1f8a834e3c7067c637e52fdcee42c4
Kamil Trzcinski <ayufan@ayufan.eu>
2016-09-18 21:43:49 +0200

    Fix realtek bluetooth support

--- tulip-chiphd/device.mk
+++ tulip-chiphd/device.mk
@@ -56,6 +56,9 @@ PRODUCT_COPY_FILES += \
     device/softwinner/tulip-chiphd/twrp.fstab:recovery/root/etc/twrp.fstab
 
 PRODUCT_COPY_FILES += \
+    device/softwinner/tulip-chiphd/bluetooth/rtkbt.conf:system/etc/bluetooth/rtkbt.conf
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
