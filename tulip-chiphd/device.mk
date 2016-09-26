065ba8ce653aca7e85758e0aed0756b2661ec8f6
Kamil Trzcinski <ayufan@ayufan.eu>
2016-09-26 13:42:32 +0200

    Add missing input.evdev.default

--- tulip-chiphd/device.mk
+++ tulip-chiphd/device.mk
@@ -39,7 +39,8 @@ endif # TARGET_KERNEL_BUILT_FROM_SOURCE
 
 PRODUCT_PACKAGES += \
     hdmi_cec.tulip \
-    power.tulip
+    power.tulip \
+    input.evdev.default
 
 DEVICE_PACKAGE_OVERLAYS := \
     device/softwinner/tulip-chiphd/overlay \
