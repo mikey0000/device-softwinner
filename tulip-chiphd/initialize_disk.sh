8125d652e1fcccb72ff22117a755cd2a4b661be7
Kamil Trzcinski <ayufan@ayufan.eu>
2016-10-05 12:37:46 +0200

    Improve the initialize_disk script

--- tulip-chiphd/initialize_disk.sh
+++ tulip-chiphd/initialize_disk.sh
@@ -1,40 +1,62 @@
 #!/bin/sh
 
-if [ -e /dev/block/mmcblk0p3 ] || [ -e /dev/block/mmcblk0p4 ]; then
+exec 1> /dev/kmsg
+exec 2> /dev/kmsg
+
+BB=/sbin/busybox
+DISK=/dev/block/mmcblk0
+PART_NO=4
+DISK_PART=${DISK}p${PART_NO}
+
+CYLINDERS=$(${BB} fdisk -l $DISK | ${BB} grep heads | ${BB} awk '{print $5}')
+START_DISK=$(${BB} fdisk -l $DISK | ${BB} grep ${DISK_PART} | ${BB} awk '{print $2}')
+END_DISK=$(${BB} fdisk -l $DISK | ${BB} grep ${DISK_PART} | ${BB} awk '{print $3}')
+
+if [ -z "$CYLINDERS" ] || [ -z "$START_DISK" ] || [ -z "$END_DISK" ]; then
+  echo "Missing fdisk data"
+  ${BB} fdisk -l $DISK
+  exit 4
+fi
+
+FDISK_SCRIPT="d
+${PART_NO}
+n
+p
+${START_DISK}
+${CYLINDERS}
+w
+p
+"
+
+if [ "${END_DISK}" != "${CYLINDERS}" ]; then
+  # Create partitions
+  if ! echo "$FDISK_SCRIPT" | ${BB} fdisk ${DISK}; then
+    echo "Failed to update mmcblk0 with fdisk"
+    exit 1
+  fi
+else
+  echo "$DISK_PART: is $START_DISK to $END_DISK of $CYLINDERS"
+fi
+
+# Mount system
+if ! ${BB} mount -t ext4 -o ro,barrier=1 ${DISK}p2 /system; then
+  echo "Failed to mount /system"
+  exit 2
+fi
+
+# Nothing to do?
+if /system/bin/resize2fs ${DISK_PART} 2>&1 | ${BB} grep "Nothing to do"; then
+  echo "Disk is already resized"
   exit 0
 fi
 
-# Create partitions
-echo "n
-p
-3
-67777
-92352
-p
-n
-p
-92353
+# Check partition
+/system/bin/e2fsck -p -f ${DISK_PART}
 
-w
-p
-" | /sbin/busybox fdisk /dev/block/mmcblk0
+# Resize partition
+if ! /system/bin/resize2fs ${DISK_PART}; then
+  echo "Failed to resize /data"
+  exit 3
+fi
 
-# Re-read partitions
-/sbin/busybox blockdev --rereadpt /dev/block/mmcblk0
-
-# Force to format partitions
-for i in 1 2 3 4 5 6 7 8 9 10; do
-  if [ -e /dev/block/mmcblk0p3 ]; then
-    /sbin/busybox dd if=/dev/zero of=/dev/block/mmcblk0p3 bs=1048576 count=1
-    break
-  fi
-  /sbin/busybox sleep 1s
-done
-
-for i in 1 2 3 4 5 6 7 8 9 10; do
-  if [ -e /dev/block/mmcblk0p4 ]; then
-    /sbin/busybox dd if=/dev/zero of=/dev/block/mmcblk0p4 bs=1048576 count=1
-    break
-  fi
-  /sbin/busybox sleep 1s
-done
+echo "Disk is resized"
